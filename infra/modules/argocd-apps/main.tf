49d428b6563fb3ad3bdfd4d81b7f71d70a10d7da
ilay1214 <ilay.elimelech2@gmail.com>
2025-11-04 17:53:02 +0200

    added deletion for finelizers

--- infra/modules/argocd-apps/main.tf
+++ infra/modules/argocd-apps/main.tf
@@ -8,4 +8,22 @@ locals {
 resource "kubernetes_manifest" "app" {
   for_each = toset(local.manifests)
   manifest = yamldecode(file(each.value))
+
+  # Handle deletion of resources with finalizers
+  wait {
+    fields = {
+      # Wait for the Application to be fully deleted
+      "metadata.deletionTimestamp" = "*"
+    }
+  }
+
+  # Force removal of finalizers on destroy
+  provisioner "local-exec" {
+    when    = destroy
+    command = <<-EOT
+      kubectl patch application ${self.manifest.metadata.name} -n ${self.manifest.metadata.namespace} \
+        --type json -p='[{"op": "remove", "path": "/metadata/finalizers"}]' 2>nul || exit 0
+    EOT
+    on_failure = continue
+  }
 }
